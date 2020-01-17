3797120b5e78f4c16a4860e8f86cfcb72a885a94
Christoph Engelmayer <git@engelmayer.me>
2020-01-17 14:11:06 +0100

    ProcessWire Module structure

--- cli.php
+++ cli.php
@@ -2,19 +2,4 @@
 namespace ProcessWire;
 include("index.php");
 
-function iteratePages($page, $callback)
-{
-    if (is_callable($callback)) {
-        call_user_func($callback, $page);
-    }
-    foreach ($page->children as $child) {
-        iteratePages($child, $callback);
-    }
-}
-
-iteratePages($pages->get("/"), function ($page) {
-    $path = './static' . $page->url;
-    mkdir($path, 0700, true);
-    file_put_contents($path."index.html", $page->render());
-    echo $page->url . "\n";
-});
+$modules->get('StaticWire')->build('/', true);
